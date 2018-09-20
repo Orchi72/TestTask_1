7b315129861d5e79a526a8d643db8094f931a2c3
Orchi72 <Orchi72@yandex.ru>
2018-09-20 12:45:10 +0500

    Добавьте файлы проекта.

---
+++ TestTask_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TestTask_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TranslateClass translater = new TranslateClass();
+
+            translater.lp.InputLang = YandexLinguistics.NET.Lang.Ru;
+            translater.lp.OutputLang = YandexLinguistics.NET.Lang.En;
+
+            string text = "";
+
+            Console.WriteLine("Привет! Я переводчик от Яндекса! Напишите текст на русском языке, для перевода на английский. Для выхода, напишите 'exit' или отправте пустой запрос.");
+
+            text = Console.ReadLine();
+
+            while ((text != "") && (text != "exit"))
+            {
+                Console.WriteLine(translater.GetTransliteText(text));
+
+                text = Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ TestTask_1/TranslateClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YandexLinguistics.NET;
+
+namespace TestTask_1
+{
+    class TranslateClass
+    {
+        public TranslateClass()
+        {
+            tr = new Translator(apiStringKey);
+            lp = new LangPair();
+        }
+
+        const string apiStringKey = "trnsl.1.1.20180911T170920Z.bbbe32530f1728ff.7fdb51f1907a42adf4eae4888d4d4081db90352e";
+        Translator tr;
+        public LangPair lp;
+
+        public string GetTransliteText(string text)
+        {
+            try
+            {
+                return tr.Translate(text, lp).Text;
+            }
+            catch (Exception ex)
+            {
+                return "Ошибка: " + ex.Message;
+            }
+        }
+    }
+}
